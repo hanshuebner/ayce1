d6b7df4b667f572af1c26d211a57d0c2347f0a68
Manuel Odendahl <wesen@ruinwesen.com>
2010-05-08 21:09:29 +0200

    added toolchain checker

---
+++ tools/check-gcc-version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TOOLCHAIN_VERSION=`avr-gcc -v 2>&1 | grep version`;
+FIXED_TOOLCHAIN_VERSION="gcc version 4.3.2 (GCC) "
+
+# ensure we always use the same version
+if [ "$TOOLCHAIN_VERSION" != "$FIXED_TOOLCHAIN_VERSION" ]; then
+		echo "$TOOLCHAIN_VERSION should be $FIXED_TOOLCHAIN_VERSION"
+		exit 1
+else
+		exit 0
+fi
