bc217a2fc410a8e5f714fd21947d5bd97fd7fc63
Manuel Odendahl <wesen@ruinwesen.com>
2010-05-09 09:23:30 +0200

    remove different block sizes from measuring script

--- tools/measure.sh
+++ tools/measure.sh
@@ -3,7 +3,11 @@
 COUNT=$((1024 * 1024))
 
 echo "* git revision" `git rev-parse HEAD`
-echo "--------------------"
-for bs in 2 4 8 16 32 64; do
+echo
+#for bs in 2 4 8 16 32 64; do
+for bs in 64; do
 		./dmx-speed.sh $bs $COUNT
+		sleep 2
 done
+echo
+echo
